db1acfe14ef613599032ff6dda67ae4ad4ad52d6
RakulAgn <rakul0agn@gmail.com>
2022-10-10 18:07:37 +0530

    ...

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
-
-import SideBar from "./Components/SideBar/SideBar";
-import TopBar from "./Components/TopBar/TopBar";
 import "./App.css"
-import Canvas from "./Components/Canvas/Canvas";
 
 function App() {
   return (
     <div className="App">
-      <TopBar/>
-      <div className="editor">
-        <Canvas/>
-        <SideBar/>
-      </div>
+     asd
     </div>
   );
 }
