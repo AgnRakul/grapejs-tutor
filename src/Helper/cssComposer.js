e235de75577d0d0c367691d8c1703e97b3ba718a
RakulAgn <rakul0agn@gmail.com>
2022-10-21 09:55:25 +0530

    ..

--- src/Helper/cssComposer.js
+++ src/Helper/cssComposer.js
@@ -1,5 +1,7 @@
 export const CustomCssComposer = (editor) => {
   editor.CssComposer.setRule(".gjs-dashed *[data-gjs-type='wrapper']", {
-    "background-color": "#80808017",
+    // "background-color": "#80808017",
+    background:
+      "url(https://github.com/RakulAgn/grapejs-tutor/blob/CustomDeviceManager-and-UI/src/Assets/Png/EditorBg.png)",
   });
 };
