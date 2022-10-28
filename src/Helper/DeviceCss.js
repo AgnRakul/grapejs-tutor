785160d13a751f31af08948c5ce36872fa83bfed
RakulAgn <rakul0agn@gmail.com>
2022-10-28 16:19:08 +0530

    ...

--- src/Helper/DeviceCss.js
+++ src/Helper/DeviceCss.js
@@ -1,29 +1,38 @@
 export const MobileCss = ` .gjs-editor {
-    width: 19%;
+    width: 100%
     font-family: Helvetica, sans-serif;
     font-size: .75rem;
     position: relative;
     background-color: transparent;
-    left: 38%;
-    right: 30% !important;
-    top: 4%;
+    left: 0;
+    right: 0;
+    top: 0;
     box-sizing: border-box;
-    height: 74%;
+    height: 100%;
 }
 
 .gjs-cv-canvas {
-    width: 100%;
-    padding: 10;
-    height: 98%;
+    width: 35%;
+    height: 100%;
     border-style: none !important;
     border: none !important;
     box-shadow: none;
+    left: 30%;
     box-sizing: initial !important;
     overflow: hidden;
-    top: 6px;
-    background-image: url(https://github.com/RakulAgn/grapejs-tutor/blob/CustomDeviceManager-and-UI/src/Assets/Svg/Mobile.svg);
+    top: 0px;
+    background-image: url(https://raw.githubusercontent.com/RakulAgn/grapejs-tutor/CustomDeviceManager-and-UI/src/Assets/Svg/Mobile.svg);
     background-repeat: no-repeat;
     background-color: transparent;
+}
+
+.gjs-frame-wrapper {
+    left: 9px !important;
+    top: 77px !important;
+    width: 376px !important;
+    height: 85.6% !important;
+    right: 0;
+    margin: auto;
 }
 
 .gjs-cv-canvas__frames {
@@ -41,20 +50,19 @@ export const MobileCss = ` .gjs-editor {
 
 .gjs-frame {
     outline: medium none;
-    height: 93%;
-    width: 105%;
+    height: 100%;
+    width: 100%;
     background-color: grey;
     border: none;
-    border-radius: 10px;
+    border-radius: 44.54px !important;
     display: block;
     transition: width .35s ease, height .35s ease;
     position: absolute;
-    top: -3px;
+    top: 6px;
     bottom: 0;
-    left: -10px;
+    left: 2px;
     right: 0;
 }
-
 `;
 
 export const DesktopCss = ` .gjs-editor {
@@ -68,6 +76,16 @@ export const DesktopCss = ` .gjs-editor {
     top: 0;
     box-sizing: border-box;
     height: 100%;
+}
+
+.gjs-frame-wrapper {
+    position: absolute !important;
+    width: 100% !important;
+    height: 100% !important;
+    left: 0 !important;
+    top: 0 !important;
+    right: 0 !important;
+    margin: auto;
 }
 
 .gjs-cv-canvas {
@@ -117,4 +135,69 @@ export const DesktopCss = ` .gjs-editor {
 
 `;
 
-export const TabletCss = ``;
+export const TabletCss = `.gjs-editor {
+    width: 100%
+    font-family: Helvetica, sans-serif;
+    font-size: .75rem;
+    position: relative;
+    background-color: transparent;
+    left: 0;
+    right: 0;
+    top: 0;
+    box-sizing: border-box;
+    height: 100%;
+}
+
+.gjs-cv-canvas {
+    width: 35%;
+    height: 100%;
+    border-style: none !important;
+    border: none !important;
+    box-shadow: none;
+    left: 30%;
+    box-sizing: initial !important;
+    overflow: hidden;
+    top: 0px;
+    background-image: url(https://raw.githubusercontent.com/RakulAgn/grapejs-tutor/50de11264bcd49199715e27738de459a5a181329/src/Assets/Svg/Tablet.svg);
+    background-repeat: no-repeat;
+    background-color: transparent;
+}
+
+.gjs-frame-wrapper {
+    left: 9px !important;
+    top: 77px !important;
+    width: 376px !important;
+    height: 85.6% !important;
+    right: 0;
+    margin: auto;
+}
+
+.gjs-cv-canvas__frames {
+    background-color: transparent;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+}
+
+.gjs-frames {
+    background-color: #f5f5f500;
+}
+
+.gjs-frame {
+    outline: medium none;
+    height: 100%;
+    width: 100%;
+    background-color: grey;
+    border: none;
+    border-radius: 44.54px !important;
+    display: block;
+    transition: width .35s ease, height .35s ease;
+    position: absolute;
+    top: 6px;
+    bottom: 0;
+    left: 2px;
+    right: 0;
+}
+`;
