3731ffbdefff7d29636d9061d40d401d966fba06
Rakul Agn <70213104+RakulAgn@users.noreply.github.com>
2022-11-04 13:11:18 +0530

    Update command-utilts.js

--- src/Helper/command-utilts.js
+++ src/Helper/command-utilts.js
@@ -107,4 +107,77 @@ export const CustomCommands = (editor) => {
       e.UndoManager.redo();
     },
   });
+  
+  /*   
+   defaultCommands['tlb-move'] = {
+        run(ed, sender, opts = {}) {
+          let dragger;
+          const em = ed.getModel();
+          const event = opts && opts.event;
+          const { target } = opts;
+          const sel = target || ed.getSelected();
+          const selAll = target ? [target] : [...ed.getSelectedAll()];
+          const nativeDrag = event && event.type == 'dragstart';
+          const defComOptions = { preserveSelected: 1 };
+          const modes = ['absolute', 'translate'];
+
+          if (!sel || !sel.get('draggable')) {
+            return em.logWarning('The element is not draggable');
+          }
+
+          const mode = sel.get('dmode') || em.get('dmode');
+          const hideTlb = () => em.stopDefault(defComOptions);
+          const altMode = includes(modes, mode);
+          selAll.forEach(sel => sel.trigger('disable'));
+
+          // Without setTimeout the ghost image disappears
+          nativeDrag ? setTimeout(hideTlb, 0) : hideTlb();
+
+          const onStart = data => {
+            em.trigger(`${eventDrag}:start`, data);
+          };
+          const onDrag = data => {
+            em.trigger(eventDrag, data);
+          };
+          const onEnd = (e, opts, data) => {
+            selAll.forEach(sel => sel.set('status', 'selected'));
+            ed.select(selAll);
+            sel.emitUpdate();
+            em.trigger(`${eventDrag}:end`, data);
+
+            // Defer selectComponent in order to prevent canvas "freeze" #2692
+            setTimeout(() => em.runDefault(defComOptions));
+
+            // Dirty patch to prevent parent selection on drop
+            (altMode || data.cancelled) && em.set('_cmpDrag', 1);
+          };
+
+          if (altMode) {
+            // TODO move grabbing func in editor/canvas from the Sorter
+            dragger = ed.runCommand('core:component-drag', {
+              guidesInfo: 1,
+              mode,
+              target: sel,
+              onStart,
+              onDrag,
+              onEnd,
+              event,
+            });
+          } else {
+            if (nativeDrag) {
+              event.dataTransfer.setDragImage(sel.view.el, 0, 0);
+              //sel.set('status', 'freezed');
+            }
+
+            const cmdMove = ed.Commands.get('move-comp');
+            cmdMove.onStart = onStart;
+            cmdMove.onDrag = onDrag;
+            cmdMove.onEndMoveFromModel = onEnd;
+            cmdMove.initSorterFromModels(selAll);
+          }
+
+          selAll.forEach(sel => sel.set('status', 'freezed-selected'));
+        },
+      };
+  */
 };
